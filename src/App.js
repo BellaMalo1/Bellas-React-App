4cd3691092ea36a8f96b4c155d8f5180c6fb7259
bellamalo1 <bmaldo23@gmail.com>
2022-07-14 16:57:24 -0700

    api fetch confirmed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [ art, setArt] = useState([])
+  
+
+  const getArt = function () {
+   
+const url = `https://api.artsy.net/api/artworks?total_count=`
+
+
+    fetch(url,{ headers:{"X-XAPP-Token":process.env.REACT_APP_KEY} } )
+      .then((res) => {
+        // parse JSON string into an actual JS object
+        return res.json()
+      })
+      .then((data) => {
+        // once we have the object, do stuff
+        console.log('success!', data)
+        setArt(data._embedded.artworks)
+        console.log(art)
+        
+      
+        
+      })
+      .catch((err) => {
+        console.log('something went wrong...', err)
+      })
+  }
+  return(
+    <>
+      <h1>Hello World</h1>
+      <button onClick={getArt}>Get Art!</button>
+      
+   
+     
+  
+
+    </>
+      
+  
+  )
+  }
+
+
 
 export default App;
+
