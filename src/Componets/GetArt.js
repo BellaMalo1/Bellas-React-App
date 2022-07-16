792e263fc96941f6c100a85f56d2693476ca5f7a
bellamalo1 <bmaldo23@gmail.com>
2022-07-15 17:30:25 -0700

    GetArt component rendering an image

--- src/Componets/GetArt.js
+++ src/Componets/GetArt.js
@@ -1,37 +1,37 @@
 
-// import { useState } from 'react'
+import { useState } from 'react'
   
-// const [ art, setArt] = useState([])
 
-   const GetArt = function () {
-    return(
-        <>
-        <h1>You're in Get Art</h1>
-        </>
-    )
 
-    
-//  const url = `https://api.artsy.net/api/artworks`
- 
- 
-//      fetch(url,{ headers:{"X-XAPP-Token":process.env.REACT_APP_KEY} } )
-//        .then((res) => {
-//          // parse JSON string into an actual JS object
-//          return res.json()
-//        })
-//        .then((data) => {
-//          // once we have the object, do stuff
-//          console.log('success!', data)
-//          setArt(data._embedded.artworks[2]._links.thumbnail.href)
-//          console.log(art)
+
+function GetArt () {
+    const [ art, setArt] = useState([])
+   
+ const url = `https://api.artsy.net/api/artworks`
+        
+ fetch(url,{ headers:{"X-XAPP-Token":process.env.REACT_APP_KEY} } )
+       .then((res) => {
+         // parse JSON string into an actual JS object
+         return res.json()
+       })
+       .then((data) => {
+         // once we have the object, do stuff
+         console.log('success!', data)
+         setArt(data._embedded.artworks[4]._links.thumbnail.href)
+         
         
  
-         
-       
-         
-//        })
-//        .catch((err) => {
-//          console.log('something went wrong...', err)
-//        })
+    })
+       .catch((err) => {
+         console.log('something went wrong...', err)
+       })
+   
+
+   return(
+    <>
+    <h1>You're in Get Art</h1>
+    <img src={art} alt="painting"/>
+    </>
+)
    }
    export default GetArt 
