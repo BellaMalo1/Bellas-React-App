14a5e214b606ce5433d5d7df9c14602ad6c4e3aa
bellamalo1 <bmaldo23@gmail.com>
2022-07-18 16:57:03 -0700

    mapped artworks

--- src/Componets/GetArt.js
+++ src/Componets/GetArt.js
@@ -1,38 +1,51 @@
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
   
 function GetArt () {
     const [ art, setArt] = useState([])
    
     const url = `https://api.artsy.net/api/artworks/`
-        
-    fetch(url,{ headers:{"X-XAPP-Token":process.env.REACT_APP_KEY} } )
+       
+    useEffect(() => {
+      fetch(url,{ headers:{"X-XAPP-Token":process.env.REACT_APP_KEY} } )
        .then((res) => {
-         // parse JSON string into an actual JS object
+        
          return res.json()
        })
        .then((data) => {
-         // once we have the object, do stuff
+        
          console.log('success!', data)
-         setArt(data._embedded.artworks[3]._links.thumbnail.href)
-        // setArt(data._embedded.artworks)
+        //  setArt(data._embedded.artworks[2]._links.thumbnail.href)
+        setArt(data._embedded.artworks)
 
     })
        .catch((err) => {
          console.log('something went wrong...', err)
        })
    
+
+    
+    },[])
+
+    useEffect(() => {
+   console.log(art[0])
+    },[art])
+   
+  if(art.length === 0) {
+    return null
+  }
+  
+    
 return (
     <div className='Art-gallery'>
-
     <h1>You're in Get Art!</h1>
     <img src={art} alt="painting"/> 
   
-{/* {art.map(artworks => (
+{art.length > 0 && art.map(artworks => (
     <div key={artworks.thumbnail} className='Gallery'>
-        <img src={artworks.thumbnail.href} alt={artworks.artist.href} /> 
+        <img src={artworks._links.thumbnail.href} alt={artworks._links.artists.href} /> 
      </div>
-))} */}
+))}
 
 
 
