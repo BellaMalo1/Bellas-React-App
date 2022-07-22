47d71931f008a8f95e4201213b4bb0a4bde9fd43
bellamalo1 <bmaldo23@gmail.com>
2022-07-21 19:50:47 -0700

    styled home page

--- src/Componets/Home.js
+++ src/Componets/Home.js
@@ -3,11 +3,11 @@ function Home (){
     return(
     <>
     <div className='Welcome-container'>
-            <h1 className="welcome-message">Welcome To Art Crawl</h1>
-   
-    <section className="info-message">
+         <img className="welcome-image" src={require('./article_topimage_michelangelo.jpeg')}alt="Michaelangelo's breath of live"></img>
+        <h1 className="welcome-message">Welcome To Art Crawl</h1>
+        <section className="info-message">
         <h2>This simple appliction is powered by Artsy API and was created as a way for art lovers to have easy acess to some of their favorite works from a variety of different Art movements</h2>
-        <img src={require('./article_topimage_michelangelo.jpeg')}alt="Michaelangelo's breath of live"></img>
+      
     </section>
     </div>
     </>
